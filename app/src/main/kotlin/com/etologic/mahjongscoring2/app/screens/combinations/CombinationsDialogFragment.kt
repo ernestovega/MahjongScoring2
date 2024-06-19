283e1e9f39df6787b75cf9f6932743422afacc7d
Ernesto.Vega <ernesto.vega@siriusxm.com>
2024-06-19 03:44:46 +0200

    Combinations converted in dialog fragment, just fails the search bar colors

--- app/src/main/kotlin/com/etologic/mahjongscoring2/app/screens/combinations/CombinationsDialogFragment.kt
+++ app/src/main/kotlin/com/etologic/mahjongscoring2/app/screens/combinations/CombinationsDialogFragment.kt
@@ -18,6 +18,8 @@
 package com.etologic.mahjongscoring2.app.screens.combinations
 
 import android.app.SearchManager
+import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -29,26 +31,32 @@ import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.SearchView
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.core.view.MenuProvider
+import androidx.core.view.children
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Lifecycle.State.STARTED
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.etologic.mahjongscoring2.R
-import com.etologic.mahjongscoring2.app.base.BaseMainFragment
 import com.etologic.mahjongscoring2.app.model.ShowState.SHOW
 import com.etologic.mahjongscoring2.app.screens.MainActivity
 import com.etologic.mahjongscoring2.app.utils.KeyboardUtils.hideKeyboard
-import com.etologic.mahjongscoring2.databinding.CombinationsFragmentBinding
+import com.etologic.mahjongscoring2.databinding.CombinationsDialogFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
 @AndroidEntryPoint
-class CombinationsFragment : BaseMainFragment() {
+class CombinationsDialogFragment : DialogFragment() {
 
     companion object {
         const val TAG = "CombinationsFragment"
@@ -57,27 +65,38 @@ class CombinationsFragment : BaseMainFragment() {
     @Inject
     lateinit var rvAdapter: CombinationsRvAdapter
 
-    private var _binding: CombinationsFragmentBinding? = null
+    private var _binding: CombinationsDialogFragmentBinding? = null
     private val binding get() = _binding!!
 
     private val viewModel: CombinationsViewModel by viewModels()
 
-    override val toolbarMenuProvider = object : MenuProvider {
+    private val whiteColor: Int by lazy { ContextCompat.getColor(requireContext(), R.color.white) }
+    private val greenColor: Int by lazy { ContextCompat.getColor(requireContext(), R.color.colorPrimary) }
+
+    private val toolbarMenuProvider = object : MenuProvider {
         override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
             menuInflater.inflate(R.menu.combinations_menu, menu)
             val searchManager = getSystemService(requireContext(), SearchManager::class.java)
-            val searchView = menu.findItem(R.id.action_search_combination)?.actionView as? SearchView
-            searchView?.setSearchableInfo(searchManager?.getSearchableInfo(requireActivity().componentName))
-            searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-                override fun onQueryTextSubmit(query: String): Boolean {
-                    return false
-                }
+            (menu.findItem(R.id.action_search_combination)?.actionView as? SearchView)?.apply {
+//                setStyle()
+                findViewById<ImageView>(androidx.appcompat.R.id.search_button)?.setColorFilter(whiteColor)
+                findViewById<ImageView>(androidx.appcompat.R.id.search_close_btn)?.setColorFilter(whiteColor)
+                findViewById<TextView>(androidx.appcompat.R.id.search_src_text)?.setTextColor(whiteColor)
+//                findViewById<ImageView>(androidx.appcompat.R.id.search_mag_icon)?.setColorFilter(greenColor)
 
-                override fun onQueryTextChange(newText: String): Boolean {
-                    viewModel.searchCombination(newText)
-                    return true
-                }
-            })
+
+                setSearchableInfo(searchManager?.getSearchableInfo(requireActivity().componentName))
+                setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String): Boolean {
+                        return false
+                    }
+
+                    override fun onQueryTextChange(newText: String): Boolean {
+                        viewModel.searchCombination(newText)
+                        return true
+                    }
+                })
+            }
         }
 
         override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
@@ -92,14 +111,26 @@ class CombinationsFragment : BaseMainFragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = CombinationsFragmentBinding.inflate(inflater, container, false)
+        _binding = CombinationsDialogFragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun getTheme(): Int {
+        return R.style.FullScreenDialogMM
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setToolbar()
         setupRecyclerView()
         startObservingViewModel()
+    }
+
+    private fun setToolbar() {
+        with(binding.toolbarCombinations) {
+            setNavigationOnClickListener { dismiss() }
+            addMenuProvider(toolbarMenuProvider, viewLifecycleOwner, Lifecycle.State.RESUMED)
+        }
     }
 
     private fun setupRecyclerView() {
@@ -130,9 +161,9 @@ class CombinationsFragment : BaseMainFragment() {
         }
     }
 
-    override fun onPause() {
+    override fun onDismiss(dialog: DialogInterface) {
         binding.root.hideKeyboard()
-        super.onPause()
+        super.onDismiss(dialog)
     }
 
     override fun onDestroyView() {
